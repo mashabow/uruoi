242aadcfb0cea4569287b1313724f526356e0b8d
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-05-22 20:20:50 +0900

    feat: get soil moisture value from the sensor

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@ const int PUMP_PIN = 4; // IO4
 void setup()
 {
   pinMode(PUMP_PIN, OUTPUT);
+  Serial.begin(74880);
 }
 
 void loop()
 {
-  digitalWrite(PUMP_PIN, HIGH);
-  delay(5000);
-  digitalWrite(PUMP_PIN, LOW);
-  delay(5000);
+  const int value = analogRead(A0);
+  Serial.println(value);
+  delay(1000);
+
+  // digitalWrite(PUMP_PIN, HIGH);
+  // delay(5000);
+  // digitalWrite(PUMP_PIN, LOW);
+  // delay(5000);
 }
