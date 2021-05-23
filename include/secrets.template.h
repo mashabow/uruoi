d9bca99e32bd3626bb96dc88ce9bb6fbcc8ea191
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-05-23 14:23:17 +0900

    feat: send soil moisture to Ambient

---
+++ include/secrets.template.h
@@ -0,0 +1,7 @@
+// secrets.h にコピーし、値を書き換えて使うこと
+
+constexpr char WIFI_SSID[] = "**********";
+constexpr char WIFI_PASSWORD[] = "**********";
+
+constexpr unsigned int AMBIENT_CHANNEL_ID = 0000000;
+constexpr char AMBIENT_WRITE_KEY[] = "**********";
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "Ambient.h"
+#include "secrets.h"
 
 const int PUMP_PIN = 4; // IO4
 
+WiFiClient client;
+Ambient ambient;
+
 void setup()
 {
-  pinMode(PUMP_PIN, OUTPUT);
   Serial.begin(74880);
+  pinMode(PUMP_PIN, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+  Serial.println("Wi-Fi connected.");
+
+  ambient.begin(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY, &client);
 }
 
 void loop()
 {
-  const int value = analogRead(A0);
-  Serial.println(value);
-  delay(1000);
+  const int vAir = 394;   // 空気中での測定値
+  const int vWater = 228; // 水中での測定値
+  const float moisture = 100.0 * (vAir - analogRead(A0)) / (vAir - vWater);
+  Serial.println(moisture);
+
+  ambient.set(1, moisture);
+  ambient.send();
+
+  delay(30 * 1000);
 
   // digitalWrite(PUMP_PIN, HIGH);
   // delay(5000);
