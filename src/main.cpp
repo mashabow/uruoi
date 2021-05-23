73df87d92b89eb1b255bcf451ffce27f68925740
Masaya Nakamura <m-nakamura@feedforce.jp>
2021-05-23 21:27:36 +0900

    feat: use deep sleep

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,55 @@ const int PUMP_PIN = 4; // IO4
 WiFiClient client;
 Ambient ambient;
 
+/* deep sleep して終了。wake 時には setup から始まる */
+void deepSleep()
+{
+  const int intervalMinutes = 10;
+  ESP.deepSleep(intervalMinutes * 60 * 1000 * 1000, WAKE_RF_DEFAULT);
+  delay(1000); // deep sleep が始まるまで待つ
+}
+
+void setupWiFi()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.println("Connecting WiFi...");
+  int retryCount = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    if (retryCount++ > 40)
+      deepSleep();
+    delay(500);
+  }
+  Serial.println("Connected.");
+}
+
+float getMoisture()
+{
+  const int vAir = 394;   // 空気中での測定値
+  const int vWater = 228; // 水中での測定値
+  return 100.0 * (vAir - analogRead(A0)) / (vAir - vWater);
+}
+
 void setup()
 {
   Serial.begin(74880);
   pinMode(PUMP_PIN, OUTPUT);
+  setupWiFi();
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-  }
-  Serial.println("Wi-Fi connected.");
-
-  ambient.begin(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY, &client);
-}
-
-void loop()
-{
-  const int vAir = 394;   // 空気中での測定値
-  const int vWater = 228; // 水中での測定値
-  const float moisture = 100.0 * (vAir - analogRead(A0)) / (vAir - vWater);
+  const float moisture = getMoisture();
   Serial.println(moisture);
 
+  ambient.begin(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY, &client);
   ambient.set(1, moisture);
   ambient.send();
-
-  delay(30 * 1000);
 
   // digitalWrite(PUMP_PIN, HIGH);
   // delay(5000);
   // digitalWrite(PUMP_PIN, LOW);
   // delay(5000);
+
+  deepSleep();
 }
+
+void loop() {}
